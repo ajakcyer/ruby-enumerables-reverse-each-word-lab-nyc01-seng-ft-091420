aac9c00dbe59d0368fe2082f942b26b0df550e38
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-06 06:07:57 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+require 'pry'
+sentence = "Hello there, and how are you?"
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  # binding.pry
+  sentence_array.map do |word|
+    word.reverse
+  end
+  binding.pry
+  sentence_array.join(" ")
+  binding.pry
+end
+binding.pry
