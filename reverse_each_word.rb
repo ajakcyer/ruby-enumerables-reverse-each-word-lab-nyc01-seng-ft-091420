91b68490b58c9a9c5981bd43847c8199eea4d2f1
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-06 06:30:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   reversed_array = []
-  sentence_array.each do |word|
+  sentence_array.collect do |word|
     reversed_array.push(word.reverse)
   end
   reversed_array.join(" ")
