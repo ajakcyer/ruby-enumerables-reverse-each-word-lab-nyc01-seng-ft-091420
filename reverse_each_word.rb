34f811377acc42bb4a95e3ba8fb6c51b69acc7e8
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-06 06:27:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,5 @@
 require 'pry'
 sentence = "Hello there, and how are you?"
 def reverse_each_word(sentence)
-  # sentence_array = sentence.split(" ")
-  # sentence_array.collect do |word|
-  sentence.split(" ").collect do |word|
-    word.reverse
-  end
+
 end
